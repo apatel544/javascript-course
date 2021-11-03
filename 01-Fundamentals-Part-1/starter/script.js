d35aaf6925ab75a15779719d44962ac6c251e23b
apatel544 <ankurpatel544@gmail.com>
2021-11-03 14:12:16 -0400

    Completing Values and Variables sections of course

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,35 @@
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log("Ankur");
+console.log(23);
+
+let firstName = "Bob ";
+console.log(firstName);
+
+let myFirstJob = "Coder";
+let myCurrentJob = "teacher";
+
+let job1 = "programmer";
+let job2 = "teacher";
+
+console.log(myCurrentJob);
+console.log(myFirstJob);
+
+/*
+Practice Assignment For JS Fundamentals - Part 1
+
+LECTURE: Values and Variables
+1. Declare variables called 'country', 'continent' and 'population' and
+assign their values according to your own country (population in millions)
+2. Log their values to the console
+*/
+
+let country = "United States of America";
+let continent = "North America";
+let population = 329.5 + " (Population in Millions)";
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
