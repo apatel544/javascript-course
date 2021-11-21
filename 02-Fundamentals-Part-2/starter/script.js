5f9547dd5c795a65fc5656a847bb2bd025797917
apatel544 <ankurpatel544@gmail.com>
2021-11-21 17:20:11 -0500

    Arrow functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -59,4 +59,20 @@ const calcAge2 = function (birthYear) {
 }
 const age2 = calcAge2(1996);
 
-console.log(age1, age2)
+// console.log(age1, age2)
+
+
+// Arrow Functions
+
+const calcAge3 = birthYear => 2037 - birthYear;
+const age3 = calcAge3(1996);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+  const age = 2037 - birthYear;
+  const retirement = 65 - age;
+  // return retirement;
+  return `${ firstName } has ${ retirement } years left before retirement!`;
+}
+
+console.log(yearsUntilRetirement(1996, 'Ankur'));
