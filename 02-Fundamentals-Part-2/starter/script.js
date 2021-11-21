e1ed5b2985289b71a5e9837af671f7a1bab01c34
apatel544 <ankurpatel544@gmail.com>
2021-11-21 13:53:51 -0500

    use strict and Adding git ignore file

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -4,3 +4,18 @@
  * Course taken by: Ankur Patel
  * Start: November 17st, 2021
  ****************************/
+'use strict';
+
+/*
+let hasDriversLicense = false;
+const passedTest = true;
+
+if (passedTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log('I can Drive!');
+
+// using 'use strict' will prevent reserved words that may be implented later
+// const interface = 'audio';
+// const private = 123;
+*/
+
+
