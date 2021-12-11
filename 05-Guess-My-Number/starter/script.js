2b2609a49d837ac524672f38520e00d30b90a4c6
apatel544 <ankurpatel544@gmail.com>
2021-12-10 21:23:50 -0500

    DOM Manipulation Practice to understand the DOM

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+console.log(document.querySelector('.message').textContent);
+
+// DOM manipulation to store a new value for the message class
+document.querySelector('.message').textContent = 'Whats the Number?!';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 20;
+
+document.querySelector('.guess').value = 13;
+console.log(document.querySelector('.guess').value);
